2af8945f0590adf905be1db8c54509bc39fb4972
mehar banu <meharshaik01@gmail.com>
2026-08-30 18:26:09 +0530

    gemini api key fixed

--- backend/career_api/career_api/settings.py
+++ backend/career_api/career_api/settings.py
@@ -21,10 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    "DJANGO_SECRET_KEY",
-    'django-insecure-v=dmw0y25_@=3#t%tphxa6$pon9w=krcl0#i#%11hgvt%96v84'
-)
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -62,7 +59,7 @@ ROOT_URLCONF = 'career_api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR.parent.parent / "frontend"],  # Add this line to include the frontend directory
+        'DIRS': [BASE_DIR / "templates"],  
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR.parent.parent / "frontend" / "static",
+    BASE_DIR / "static",
 ]
 
 
